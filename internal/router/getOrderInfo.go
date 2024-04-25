4bce15e9f1321e367941139f39f85468bf110865
iliamikado <iliamikado@gmail.com>
2024-04-25 15:20:05 +0300

    fix

--- internal/router/getOrderInfo.go
+++ internal/router/getOrderInfo.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"context"
 	"encoding/json"
 	"io"
 	"net/http"
@@ -12,31 +13,19 @@ import (
 	"github.com/iliamikado/gophermarket/internal/models"
 )
 
+const MaxResponseTime = time.Second * 3
 const RepeatRequestTime = time.Second * 3
 const RepeatRequestTimeOn429 = time.Minute
 
 func updateOrderInfo(order models.Order) {
-	resp, err := http.Get(config.AccrualSystemAddress + order.Number)
-	if resp != nil {
-		defer resp.Body.Close()
-	}
 	logger.Log("Get info for order " + order.Number)
 	
-	var newOrder models.Order
-	if err != nil || resp.StatusCode != 200 {
-		logger.Log(err)
-		logger.Log(resp.Status)
-		newOrder = order
-	} else {
-		body, _ := io.ReadAll(resp.Body)
-		json.Unmarshal(body, &newOrder)
-		newOrder.Number = order.Number
-	}
+	newOrder, needWait := getOrderInfo(order)
 
 	if newOrder.Status != "PROCESSED" && newOrder.Status != "INVALID" {
 		go func(order models.Order) {
 			var sleep = RepeatRequestTime
-			if resp.StatusCode == http.StatusTooManyRequests {
+			if needWait {
 				sleep = RepeatRequestTimeOn429
 			}
 			time.Sleep(sleep)
@@ -49,4 +38,31 @@ func updateOrderInfo(order models.Order) {
 	if order != newOrder {
 		db.UpdateOrder(newOrder)
 	}
+}
+
+func getOrderInfo(order models.Order) (models.Order, bool) {
+	client := &http.Client{}
+	ctx, cancel := context.WithTimeout(context.Background(), MaxResponseTime)
+	defer cancel()
+	req, err1 := http.NewRequestWithContext(ctx, "GET", config.AccrualSystemAddress + order.Number, nil)
+	if err1 != nil {
+		return order, false
+	}
+	resp, err2 := client.Do(req)
+	if err2 != nil {
+		return order, false
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusNoContent {
+		return order, false
+	}
+	if resp.StatusCode == http.StatusTooManyRequests {
+		return order, true
+	}
+
+	var newOrder models.Order
+	body, _ := io.ReadAll(resp.Body)
+	json.Unmarshal(body, &newOrder)
+	newOrder.Number = order.Number
+	return newOrder, false
 }
